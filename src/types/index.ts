bc80bacb31e5f3827867f0ef6e419b57322d8143
Siro Daves <sirodevske@gmail.com>
2025-08-23 22:17:21 +0300

    second commit

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type Role = 'user' | 'admin';
+
+export type Book = {
+  book_id: string;
+  title: string;
+  author: string;
+  description: string;
+  created_at: string;
+  stocks: number;
+};
