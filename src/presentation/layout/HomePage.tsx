897bf4594fdd5dee3029553f8350e739292d0da9
Siro Devs <sirodevske@gmail.com>
2026-01-21 14:33:45 +0300

    groups state fixed

--- src/presentation/layout/HomePage.tsx
+++ src/presentation/layout/HomePage.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useSelector } from "react-redux";
 import Link from "next/link";
+import { useSelector } from "react-redux";
+
 import { RootState } from "@/application/state/store";
 import { Button } from "@/presentation/components/ui";
 import Faq from "../components/common/faq";
@@ -9,12 +10,13 @@ import Hero from "../components/common/hero";
 import Dashboard from "./Dashboard";
 
 export default function HomePage() {
-  const { isAuthenticated, user } = useSelector(
-    (state: RootState) => state.auth,
-  );
+  const { isAuthenticated } = useSelector((state: RootState) => state.auth);
+  
   return (
     <main className="flex flex-col min-h-dvh">
-      {!isAuthenticated && (
+      {isAuthenticated ? (
+        <Dashboard />
+      ) : (
         <>
           <Hero />
           <div className="flex flex-col justify-center items-center sm:flex-row gap-4 justify-center mt-8">
@@ -27,17 +29,9 @@ export default function HomePage() {
               </Button>
             </Link>
           </div>
-          {/* <Partners />
-      <Testimonials />
-      <Stats />
-      <Pricing /> */}
           <Faq />
         </>
       )}
-      {isAuthenticated && (
-        <Dashboard/>
-      )}
     </main>
   );
-
 }
--- src/presentation/providers/ReduxProvider.tsx
+++ src/presentation/providers/ReduxProvider.tsx
@@ -1,13 +1,23 @@
 "use client";
 
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/lib/integration/react';
-import { store, persistor } from '@/application/state/store';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import { store, persistor } from "@/application/state/store";
+import LoadingSpinner from "../components/ui/loading-spinner";
+import { AppThemeProvider } from "./AppThemeProvider";
+
+function PersistLoader() {
+  return (
+    <AppThemeProvider>
+      <LoadingSpinner />
+    </AppThemeProvider>
+  );
+}
 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      <PersistGate loading={<PersistLoader />} persistor={persistor}>
         {children}
       </PersistGate>
     </Provider>
