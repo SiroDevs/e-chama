b550feb22c263587f9d2cc459bfae99937d32a13
Siro Devs <sirodevske@gmail.com>
2026-03-12 16:35:39 +0300

    imporving the table ui

---
+++ src/domain/repositories/supabase/supabase.repo.ts
@@ -0,0 +1,59 @@
+import { Contribution, Group, GroupMember, Member, Permission, Profile } from "../../entities";
+import { BaseRepo } from "./base.repo";
+import { BaseSupabaseRepo } from "./base.supabase.repo";
+
+export class ContributionSupabaseRepo 
+  extends BaseSupabaseRepo<Contribution> 
+  implements BaseRepo<Contribution> {
+  constructor() {
+    super('contributions');
+  }
+}
+
+// export class GroupContributionSupabaseRepo 
+//   extends BaseSupabaseRepo<GroupContribution> 
+//   implements BaseRepo<GroupContribution> {
+//   constructor() {
+//     super('group_contributions');
+//   }
+// }
+
+export class GroupMemberSupabaseRepo 
+  extends BaseSupabaseRepo<GroupMember> 
+  implements BaseRepo<GroupMember> {
+  constructor() {
+    super('group_members');
+  }
+}
+
+export class GroupSupabaseRepo 
+  extends BaseSupabaseRepo<Group> 
+  implements BaseRepo<Group> {
+  constructor() {
+    super('groups');
+  }
+}
+
+export class MemberSupabaseRepo 
+  extends BaseSupabaseRepo<Member> 
+  implements BaseRepo<Member> {
+  constructor() {
+    super('members');
+  }
+}
+
+export class PermissionSupabaseRepo 
+  extends BaseSupabaseRepo<Permission> 
+  implements BaseRepo<Permission> {
+  constructor() {
+    super('permissions');
+  }
+}
+
+export class ProfileSupabaseRepo 
+  extends BaseSupabaseRepo<Profile> 
+  implements BaseRepo<Profile> {
+  constructor() {
+    super('profiles');
+  }
+}
