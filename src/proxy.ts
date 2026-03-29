b04ff61aa1667123cc666776cfa62d668ec28ac5
Siro Devs <sirodevske@gmail.com>
2026-03-29 20:13:52 +0300

    fix dashboard and middleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -13,7 +13,7 @@ const protectedRoutes = [
   "/profile"
 ];
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const userInfo = await getCurrentUser();
