74154c09a9a84ad05d43ec7626920c88faca2412
Siro Devs <sirodevske@gmail.com>
2026-03-31 23:55:37 +0300

    meetings page update

--- src/domain/entities/meeting.ts
+++ src/domain/entities/meeting.ts
@@ -1,5 +1,3 @@
-import { DatabaseFilters } from "@/types/general";
-
 export interface Meeting {
   id?: string;
   creator?: string;
@@ -15,19 +13,14 @@ export interface Meeting {
 export interface GroupMeeting {
   id?: string;
   creator?: string;
-  group_id?: string;
-  title?: string;
-  amount?: number;
-  date?: string;
-  location?: string;
-  status?: string;
-  attachment?: string;
-  description?: string;
-  note?: string;
-  user_id?: string;
   full_name?: string;
   member_no?: string;
   role?: string;
+  group_id?: string;
+  title?: string;
+  description?: string;
+  date?: string;
+  location?: string;
   created_at?: string;
   updated_at?: string;
 }
